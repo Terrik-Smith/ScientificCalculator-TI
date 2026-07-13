6c26b01fb7c7f2c25baf164beff433288a9d1966
terrik <terrik_smith@yahoo.com>
2026-07-13 12:04:11 -0400

    Restore memory, display, trig, unit, and log functions

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -1,30 +1,16 @@
 package com.zipcodewilmington.scientificcalculator;
 
-/**
- * Created by leon on 2/9/18.
- */
 public class MainApplication {
     public static void main(String[] args) {
-<<<<<<< HEAD
     
         ScientificCalculator calculator = new ScientificCalculator();
 
         calculator.setDisplay(45);
 
-        calculator.tangent();
+        calculator.add(5);
 
         Console.println("Result: %s", calculator.getDisplay());   
 
     }
-=======
-
-    ScientificCalculator calculator = new ScientificCalculator();
-
-    calculator.setDisplay(5);
-
-    calculator.switchSign();
-
-    Console.println("Result: %s", calculator.getDisplay());
 }
->>>>>>> master
-}
+
